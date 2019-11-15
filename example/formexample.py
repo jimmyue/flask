e8244f9e8531af244e47944ef24c080a01115e13
jimmyue <jimmy_job@126.com>
2019-11-15 17:53:43 +0800

    modfy form

--- example/formexample.py
+++ example/formexample.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template, request, flash
-from forms import ContactForm
+from flask_wtf import Form
+from wtforms import TextField, IntegerField, TextAreaField, SubmitField, RadioField,SelectField
+from wtforms import validators, ValidationError
 app = Flask(__name__)
 app.secret_key = 'development key'
+
+class ContactForm(Form):
+	name = TextField("Name Of Student",[validators.Required("Please enter your name.")])
+	Gender = RadioField('Gender', choices = [('M','Male'),('F','Female')])
+	Address = TextAreaField("Address",[validators.Required("Please enter your Address.")])
+	email = TextField("Email",[validators.Email("Please enter your Email.")])
+	Age = IntegerField("Age",[validators.Required("Please enter your age.")])
+	language = SelectField('Languages', choices = [('cpp', 'C++'), ('py', 'Python')])
+	submit = SubmitField("Send")
 
 @app.route('/contact', methods = ['GET', 'POST'])
 def contact():
